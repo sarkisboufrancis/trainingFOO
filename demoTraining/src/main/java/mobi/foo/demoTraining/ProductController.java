9eeb502e0c66cd5cb77dd6c567c42efb6e791db4
sarkisboufrancis <99386727+sarkisboufrancis@users.noreply.github.com>
2023-05-30 17:54:22 +0300

    ex4

--- demoTraining/src/main/java/mobi/foo/demoTraining/ProductController.java
+++ demoTraining/src/main/java/mobi/foo/demoTraining/ProductController.java
@@ -2,7 +2,6 @@ package mobi.foo.demoTraining;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
--- demoTraining/src/main/java/mobi/foo/demoTraining/ProductService.java
+++ demoTraining/src/main/java/mobi/foo/demoTraining/ProductService.java
@@ -2,6 +2,9 @@ package mobi.foo.demoTraining;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import java.util.List;
@@ -14,19 +17,19 @@ public class ProductService {
 
     private final ProductRepository productRepository;
     private final ProductDTOMapper productDTOMapper;
-
+    @Cacheable("Product")
     public List<ProductDTO> findAll() {
         return productRepository.findAll().stream().map(productDTOMapper).collect(Collectors.toList());
     }
-
+    @Cacheable(value="Product",key = "#id")
     public Optional<ProductDTO> findById(Long id) {
         return productRepository.findById(id).map(productDTOMapper);
     }
-
+    @CachePut(value = "Product",key = "#product.id")
     public Product save(Product product) {
         return productRepository.save(product);
     }
-
+    @CacheEvict(value = "Product",key = "#id")
     public void delete(Long id) {
         productRepository.deleteById(id);
     }
