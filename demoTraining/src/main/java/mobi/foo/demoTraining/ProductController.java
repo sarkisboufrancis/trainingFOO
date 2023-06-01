f4c14dc2252b1e57267b11a1f5261e8cff744e33
sarkisboufrancis <99386727+sarkisboufrancis@users.noreply.github.com>
2023-06-01 12:22:04 +0300

    Swagger

--- demoTraining/src/main/java/mobi/foo/demoTraining/ProductController.java
+++ demoTraining/src/main/java/mobi/foo/demoTraining/ProductController.java
@@ -1,6 +1,7 @@
 package mobi.foo.demoTraining;
 
 
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -18,22 +19,26 @@ public class ProductController {
     private final ProductService productService;
 
     @GetMapping
+    @Operation(summary = "Get  all product")
     public CompletableFuture<List<ProductDTO>> getAllProducts() {
         return productService.findAll();
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Get  a product by ID")
     public CompletableFuture<ResponseEntity<ProductDTO>> getProductById(@PathVariable("id") Long id) throws InterruptedException {
         return productService.findByIdAsync(id)
                 .thenApply(productDTO -> productDTO.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build()));
     }
 
     @PostMapping("/add")
+    @Operation(summary = "add a product")
     public CompletableFuture<Product> createProduct(@RequestBody @Valid Product product) {
         return productService.saveAsync(product);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "update a product by ID")
     public CompletableFuture<ResponseEntity<Product>> updateProduct(@PathVariable("id") Long id, @RequestBody Product product) {
         product.setId(id);
         return productService.saveAsync(product)
@@ -42,6 +47,7 @@ public class ProductController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Delete  a product by ID")
     public CompletableFuture<ResponseEntity<Void>> deleteProduct(@PathVariable("id") Long id) {
         return productService.deleteAsync(id)
                 .thenApply(ignore -> ResponseEntity.noContent().build());
