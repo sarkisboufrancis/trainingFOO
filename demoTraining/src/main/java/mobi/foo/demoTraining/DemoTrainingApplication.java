2550247a90b48ffcee789f06ac770f589f3f5803
sarkisboufrancis <99386727+sarkisboufrancis@users.noreply.github.com>
2023-06-06 11:01:52 +0300

    logging

--- demoTraining/src/main/java/mobi/foo/demoTraining/DemoTrainingApplication.java
+++ demoTraining/src/main/java/mobi/foo/demoTraining/DemoTrainingApplication.java
@@ -1,13 +1,27 @@
 package mobi.foo.demoTraining;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoTrainingApplication {
+	private static final Logger logger = LoggerFactory.getLogger(DemoTrainingApplication.class);
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoTrainingApplication.class, args);
-	}
+		logger.trace("This is a trace message");
+		logger.debug("This is a debug message");
+		logger.info("This is an info message");
+		logger.warn("This is a warning message");
+		logger.error("This is an error message");
 
+		SpringApplication.run(DemoTrainingApplication.class, args);
+//
+//		try {
+//			int result = 10 / 0; // Simulating an exception
+//		} catch (Exception e) {
+//			logger.error("An error occurred", e);
+//		}
+	}
 }
